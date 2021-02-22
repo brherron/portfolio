aca8b23bd6680b9ec3f2e02595c181111c9d67a0
Beau Herron <beauh@tkc32m.com>
2021-02-22 10:52:26 -0600

    ReCaptcha, maybe?

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import '../styles/styles.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 import SiteLogo from '../images/main-logo.svg'
 import { faUndoAlt, faTimes } from '@fortawesome/free-solid-svg-icons'
 import Avatar from '../images/avatar.svg'
 
 const ContactPage = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  function clearForm(e) {
+    e.preventDefault()
+    setName('')
+    setEmail('')
+    setMessage('')
+  }
+
   
   return (
     <div className="contact">
@@ -21,7 +32,7 @@ const ContactPage = () => {
             <div className="navbar-end">
               <div className="navbar-item">
                 <p className="buttons">
-                  <button className="button is-medium tooltip is-tooltip-bottom" data-tooltip="Reset" onClick="document.getElementById('form').reset()">
+                  <button className="button is-medium has-tooltip-bottom" data-tooltip="Reset" onClick={clearForm}>
                     <span className="icon is-small">
                       <FontAwesomeIcon icon={faUndoAlt} />
                     </span>
@@ -52,39 +63,43 @@ const ContactPage = () => {
         </section>
         <section className="is-white">
           <div className="container">
-            <form name="Contact Form" method="POST" data-netlify="true">
+            <form name="Contact Form" method="POST" data-netlify="true" data-netlify-recaptcha="true">
               <input type="hidden" name="form-name" value="Contact Form" />
               <div className="columns is-centered">
                 <div className="column is-half">
                   <div className="field">
-                    <label className="label">Name</label>
-                    <div className="control is-expanded">
-                      <input type="text" name="name" className="input is-large" required/>
-                    </div>
+                    <label className="label">Name
+                      <div className="control is-expanded">
+                        <input type="text" name="name" className="input is-large" value={name} onChange={(e) => setName(e.target.value)} required/>
+                      </div>
+                    </label>
                   </div>
                 </div>
                 <div className="column is-half">
                   <div className="field">
-                    <label className="label">Email</label>
-                    <div className="control is-expanded">
-                      <input type="email" name="email" className="input is-large" required/>
-                    </div>
+                    <label className="label">Email
+                      <div className="control is-expanded">
+                        <input type="email" name="email" className="input is-large" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                      </div>
+                    </label>
                   </div>
                 </div>
               </div>
               <div className="columns is-centered">
                 <div className="column">
                   <div className="field">
-                    <label className="label">Message</label>
-                    <div className="control is-expanded">
-                      <textarea name="message" className="textarea is-large" rows="5" required/>
-                    </div>
+                    <label className="label">Message
+                      <div className="control is-expanded">
+                        <textarea name="message" className="textarea is-large" rows="5" value={message} onChange={(e) => setMessage(e.target.value)} required/>
+                      </div>
+                    </label>
                   </div>
                 </div>
               </div>
               <div className="columns is-centered">
                 <div className="column is-one-third">
                   <div className="field">
+                    <div data-netlify-recaptcha="true"></div>
                     <div className="control">
                       <button className="button is-black is-outlined is-medium is-fullwidth is-rounded" type="submit">Submit</button>
                     </div>
